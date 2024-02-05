cbb3f3088a372e8de87dbc1eb6dbb8fdd587bc0a
mohamed <mohamadabdelhamed14@gmail.com>
2024-02-05 05:55:24 -0800

    first two tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+def lookup(obj):
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        print(sorted(self))
